686aae8d3e28d96b2e991935b4337bed09578d87
AnilReddy Perugu <anilreddyperugu@AnilReddys-MacBook-Air.local>
2026-07-02 14:05:20 +0530

    Final Commit

--- 03. Personal AI Assistant/src/chatbot.py
+++ 03. Personal AI Assistant/src/chatbot.py
@@ -1,6 +1,6 @@
 import os
-from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_chroma import Chroma
+# from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -8,30 +8,33 @@ load_dotenv() #loading .env
 # print(os.getenv("GOOGLE_API_KEY"))
 
 current_diectory = os.path.dirname(os.path.abspath(__file__))
-persist_db_dir = os.path.join(current_diectory, "chroma_db")
+KB_path = os.path.join(current_diectory, "..","Knowledge_base", "Knowledge_base.md")
+# persist_db_dir = os.path.join(current_diectory, "chroma_db")
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-embedding_model = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+with open(KB_path, "r", encoding="utf-8") as f:
+    markdown_file = f.read()
 
-vector_store = Chroma(persist_directory=persist_db_dir, embedding_function=embedding_model)
+# embedding_model = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+# vector_store = Chroma(persist_directory=persist_db_dir, embedding_function=embedding_model)
 
-prompt = input('Hey there! how may I help you?: ')
+context = markdown_file
 
-
+# prompt = input('Hey there! how may I help you?: ')
 def ask_question(query):
-    print("Question received:", query)
+#     print("Question received:", query)
 
-    print("Starting retrieval...")
-    matching_docs = vector_store.similarity_search(query, k=5) #Retriving the chunks
-    print("Retrieval complete")
+#     print("Loading KB...")
+#     # matching_docs = vector_store.similarity_search(query, k=5) #Retriving the chunks
+#     print("Retrieval complete")
 
     #Convert chunks into one context string
-    context = "\n\n".join(
-    [
-        f"Metadata: {doc.metadata}\nContent: {doc.page_content}"
-        for doc in matching_docs
-    ])
+    # context = "\n\n".join(
+    # [
+    #     f"Metadata: {doc.metadata}\nContent: {doc.page_content}"
+    #     for doc in matching_docs
+    # ])
     # print(context)
 
     prompt = f""" 
@@ -45,5 +48,10 @@ def ask_question(query):
     response = llm.invoke(prompt)
     return response.content
 
-answer = ask_question(prompt)
-print(answer)
+# answer = ask_question(prompt)
+# print(answer)
+
+# if __name__ == "__main__":
+#     query = input("Hey there! How may I help you?: ")
+#     answer = ask_question(query)
+#     print(answer)
